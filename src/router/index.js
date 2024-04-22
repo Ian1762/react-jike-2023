d8cfe664fbb28d86607c448e7f5ca2db9fc9178b
MacBook <macbook@192.168.2.122>
2024-04-22 17:49:02 +0800

    配置别名路径

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import {
     createBrowserRouter,
 } from "react-router-dom";
 
-import Layout from "../pages/Layout";
-import Login from "../pages/Login";
+import Layout from "@/pages/Layout";
+import Login from "@/pages/Login";
 
 const router = createBrowserRouter([
     {
