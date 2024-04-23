80b7c98cfc3a08ec96f1942831b6741a52f83310
MacBook <macbook@192.168.2.122>
2024-04-23 16:58:16 +0800

    登陆-表单校验实现

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,27 +3,45 @@ import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
 
 const Login = () => {
-  return (
-    <div className="login">
-      <Card className="login-container">
-        <img className="login-logo" src={logo} alt="" />
-        {/* 登录表单 */}
-        <Form>
-          <Form.Item>
-            <Input size="large" placeholder="请输入手机号" />
-          </Form.Item>
-          <Form.Item>
-            <Input size="large" placeholder="请输入验证码" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" size="large" block>
-              登录
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
-  )
+    return (
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt="" />
+                {/* 登录表单 */}
+                <Form validateTrigger="onBlur">
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your username!',
+                            },
+                            {
+                                pattern: /^1[3-9]\d{9}$/,
+                                message: 'Please input your mobile number!',
+                            },
+                        ]}>
+                        <Input size="large" placeholder="请输入手机号" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}>
+                        <Input size="large" placeholder="请输入验证码" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
 }
 
 export default Login
