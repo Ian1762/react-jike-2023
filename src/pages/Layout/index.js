07bd4f6c23d4fb2c01693f16127beef7d477220d
MacBook <macbook@192.168.2.122>
2024-04-26 22:09:50 +0800

    点击菜单跳转路由

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,61 +1,67 @@
 import { Layout, Menu, Popconfirm } from 'antd'
 import {
-  HomeOutlined,
-  DiffOutlined,
-  EditOutlined,
-  LogoutOutlined,
+    HomeOutlined,
+    DiffOutlined,
+    EditOutlined,
+    LogoutOutlined,
 } from '@ant-design/icons'
 import './index.scss'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const { Header, Sider } = Layout
 
 const items = [
-  {
-    label: '首页',
-    key: '1',
-    icon: <HomeOutlined />,
-  },
-  {
-    label: '文章管理',
-    key: '2',
-    icon: <DiffOutlined />,
-  },
-  {
-    label: '创建文章',
-    key: '3',
-    icon: <EditOutlined />,
-  },
+    {
+        label: '首页',
+        key: '/',
+        icon: <HomeOutlined />,
+    },
+    {
+        label: '文章管理',
+        key: 'article',
+        icon: <DiffOutlined />,
+    },
+    {
+        label: '创建文章',
+        key: 'publish',
+        icon: <EditOutlined />,
+    },
 ]
 
 const GeekLayout = () => {
-  return (
-    <Layout>
-      <Header className="header">
-        <div className="logo" />
-        <div className="user-info">
-          <span className="user-name">柴柴老师</span>
-          <span className="user-logout">
-            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
-              <LogoutOutlined /> 退出
-            </Popconfirm>
-          </span>
-        </div>
-      </Header>
-      <Layout>
-        <Sider width={200} className="site-layout-background">
-          <Menu
-            mode="inline"
-            theme="dark"
-            defaultSelectedKeys={['1']}
-            items={items}
-            style={{ height: '100%', borderRight: 0 }}></Menu>
-        </Sider>
-        <Layout className="layout-content" style={{ padding: 20 }}>
-          <Outlet></Outlet>
+    const navigate = useNavigate()
+    const onMenuClick = (route) => {
+        // console.log(route)
+        navigate(route.key)
+    }
+    return (
+        <Layout>
+            <Header className="header">
+                <div className="logo" />
+                <div className="user-info">
+                    <span className="user-name">柴柴老师</span>
+                    <span className="user-logout">
+                        <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+                            <LogoutOutlined /> 退出
+                        </Popconfirm>
+                    </span>
+                </div>
+            </Header>
+            <Layout>
+                <Sider width={200} className="site-layout-background">
+                    <Menu
+                        mode="inline"
+                        theme="dark"
+                        defaultSelectedKeys={['1']}
+                        onClick={onMenuClick}
+                        items={items}
+                        style={{ height: '100%', borderRight: 0 }}></Menu>
+                </Sider>
+                <Layout className="layout-content" style={{ padding: 20 }}>
+                    <Outlet></Outlet>
+                </Layout>
+            </Layout>
         </Layout>
-      </Layout>
-    </Layout>
-  )
+    )
 }
 export default GeekLayout
