bc730047f41d1757e31fcc9648156f70fb998e3e
MacBook <macbook@192.168.2.122>
2024-04-29 22:16:32 +0800

    准备富文本编辑器

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ import 'normalize.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
